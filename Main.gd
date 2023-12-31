f8cdae579ab770dc7ba36234587acc3d473b3b5a
TurtleWolfe <WaynePohlner@gmail.com>
2023-12-31 11:18:22 -0500

    Section Four, working game Coin Dash

--- Main.gd
+++ Main.gd
@@ -11,7 +11,7 @@ func _ready():
 	screensize = get_viewport().get_visible_rect().size
 	$Player.screensize = screensize
 	$Player.hide()
-	new_game()
+	#new_game()
 
 func new_game():
 	playing = true
@@ -22,6 +22,8 @@ func new_game():
 	$Player.show()
 	$GameTimer.start()
 	spawn_coins()
+	$HUD.update_score(score)
+	$HUD.update_timer(time_left)
 
 func spawn_coins():
 	for i in level + 4:
@@ -36,3 +38,25 @@ func _process(delta):
 		level += 1
 		time_left += 5
 		spawn_coins()
+
+func _on_game_timer_timeout():
+	time_left -= 1
+	$HUD.update_timer(time_left)
+	if time_left <= 0:
+		game_over()
+
+func _on_player_hurt():
+	game_over()
+func _on_player_pickup():
+	score += 1
+	$HUD.update_score(score)
+
+func game_over():
+	playing = false
+	$GameTimer.stop()
+	get_tree().call_group("coins", "queue_free")
+	$HUD.show_game_over()
+	$Player.die()		
+
+func _on_hud_start_game():
+	new_game()
